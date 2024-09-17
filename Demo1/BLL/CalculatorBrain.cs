e3d4c10acba24f3fd5d4c65fe411a95a4e58dd0e
Meeli <meeli.sirvi@tptlive.ee>
2024-09-17 20:20:10 +0300

    initial

---
+++ Demo1/BLL/CalculatorBrain.cs
@@ -0,0 +1,13 @@
+﻿namespace BLL;
+
+public class CalculatorBrain
+{
+    public int Number1 { get; set; }
+    public int Number2 { get; set; }
+
+    public int Add()
+    {
+        return Number1 + Number2;
+    }
+}
+
