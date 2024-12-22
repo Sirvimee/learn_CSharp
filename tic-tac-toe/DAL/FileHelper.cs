c22aed5f6a514c0ab3dcdcb2ac4cafa16e67bacb
Meeli <meeli.sirvi@tptlive.ee>
2024-12-22 18:10:59 +0200

    fixed

--- tic-tac-toe/DAL/FileHelper.cs
+++ tic-tac-toe/DAL/FileHelper.cs
@@ -5,8 +5,4 @@ public class FileHelper
     public static string BasePath = Environment
                                         .GetFolderPath(System.Environment.SpecialFolder.UserProfile)
                                     + Path.DirectorySeparatorChar + "tic-tac-two" + Path.DirectorySeparatorChar;
-
-
-    public static string ConfigExtension = ".config.json";
-    public static string GameExtension = ".json";
 }
